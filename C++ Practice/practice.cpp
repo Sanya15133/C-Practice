6de223b1e94d0c5afd6f6cfe1b4cec1175039324
Sanya Hussain <sanz_133@hotmail.com>
2026-06-14 08:58:54 +0100

    testing commits

--- C++ Practice/practice.cpp
+++ C++ Practice/practice.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
     std::cout << "Here is some text,";
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore any characters in the input buffer until newline
+    std::cin.get(); // get 1 more char from user = wait for user to press enter
     return 0;
 }
+
