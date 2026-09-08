470bb78cfc1c2a43fcc2c5a333167c171a2b0985
Sanya Hussain <sanz_133@hotmail.com>
2026-09-08 11:29:18 +0100

    1.5

--- C++ Practice/practice1.5.cpp
+++ C++ Practice/practice1.5.cpp
@@ -33,9 +33,20 @@
 //     return 0;
 // }
 
+// int main()
+//     {
+//         std::cout << "Hi!" << std::endl;
+//         std::cout << "My name is..." << std::endl;
+//         return 0;
+//     }
+
 int main()
-    {
-        std::cout << "Hi!" << std::endl;
-        std::cout << "My name is..." << std::endl;
-        return 0;
-    }
+{
+    std::cout << "Enter a number: "; // ask user for a number
+
+    int x{};       // define variable x to hold user input (and value-initialize it)
+    std::cin >> x; // get number from keyboard and store it in variable x
+
+    std::cout << "You entered " << x << '\n';
+    return 0;
+}
